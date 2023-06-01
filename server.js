5badd8583fce500f2cd6ed7c29f915ac506f60d2
Francislie <mfrancislie@gmail.com>
2023-06-01 11:10:23 -0700

    added complete order raw number

--- server.js
+++ server.js
@@ -49,6 +49,51 @@ app.get('/api/categories', (req, res) => {
   res.send(data.categories);
 });
 
+const Order = mongoose.model(
+  'Order',
+  new mongoose.Schema(
+    {
+      number: { type: Number, default: 0 },
+      orderType: String,
+      paymentType: String,
+      isPaid: { type: Boolean, default: false },
+      isReady: { type: Boolean, default: false },
+      inProgress: { type: Boolean, default: true },
+      isCanceled: { type: Boolean, default: false },
+      isDelivered: { type: Boolean, default: false },
+      itemsPrice: Number,
+      taxPrice: Number,
+      totalPrice: Number,
+      orderItems: [
+        {
+          name: String,
+          price: Number,
+          quantity: Number,
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  )
+);
+
+// Post new order and sorting Order Number
+app.post('/api/orders', async (req, res) => {
+  const lastOrder = await Order.find().sort({ number: -1 }).limit(1);
+  const lastNumber = lastOrder.length === 0 ? 0 : lastOrder[0].number;
+  if (
+    !req.body.orderType ||
+    !req.body.paymentType ||
+    !req.body.orderItems ||
+    req.body.orderItems.length === 0
+  ) {
+    return res.send({ message: 'Data is required.' });
+  }
+  const order = await Order({ ...req.body, number: lastNumber + 1 }).save();
+  res.send(order);
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
